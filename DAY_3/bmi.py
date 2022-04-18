2724f020711aabc51b157b9aa2265567249fb95c
Ruth Chepkemoi Naibei <61555911+RuthNaibei@users.noreply.github.com>
2022-04-18 09:34:26 +0000

    BMI calc

--- DAY_3/bmi.py
+++ DAY_3/bmi.py
@@ -8,11 +8,11 @@ BMI = round(weight/height**2)
 
 if BMI < 18.5:
   print(f"Your BMI is {BMI}, you are underweight. ")
-elif 18.5<BMI<25:
+elif BMI<25:
   print(f"Your BMI is {BMI}, you are have a normal weight. ")
-elif 25<BMI<30:
+elif BMI<30:
   print(f"Your BMI is {BMI}, you are slightly overweight. ")
-elif 30<BMI<35:
+elif BMI<35:
   print(f"Your BMI is {BMI}, you are obese. ")
 else:
   print(f"Your BMI is {BMI}, you are clinically obese.")
