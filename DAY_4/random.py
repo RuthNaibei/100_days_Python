b86ba0f93bc1bc2eea23104db16289e1a0246516
Ruth Chepkemoi Naibei <61555911+RuthNaibei@users.noreply.github.com>
2022-04-20 20:44:47 +0000

    heads or tails

---
+++ DAY_4/random.py
@@ -0,0 +1,24 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random 
+
+random=random.randint(0,1)
+
+if random==0:
+  print("Heads")
+else:
+  print("Tails")
+
+
+
+
+
+
+
+
+
+
+
+
+
