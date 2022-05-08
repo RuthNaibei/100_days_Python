b1c3348b4984d8a581c4a25d92d225558937704e
Ruth Chepkemoi Naibei <61555911+RuthNaibei@users.noreply.github.com>
2022-05-08 13:56:24 +0000

    Caesar's encryption

---
+++ DAY_8/encrypt.py
@@ -0,0 +1,16 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(shift_amount,text_input):
+  cipher_text=""
+  for letter in text:
+    position=alphabet.index(letter)
+    new_position= position+shift_amount
+    letter = alphabet[new_position]
+    cipher_text += letter
+  print(f"The encoded text is {cipher_text}")
+
+encrypt(shift_amount=shift,text_input=text)
